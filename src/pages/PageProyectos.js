049106f51135f6f834dd4e02eb0651c3691e7620
47297659 <47297659@A-PHZ2-CIDI-012>
2023-10-24 09:43:50 -0300

    Agregado JSON con DATA

--- src/pages/PageProyectos.js
+++ src/pages/PageProyectos.js
@@ -1,80 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import "../styles/styles.css";
 
 const PageProyectos = () => {
+  const [proyectos, setProyectos] = useState([]);
+
+  useEffect(() => {
+    axios.get('../../micaela.json')
+      .then(response => {
+        setProyectos(response.data);
+        console.data(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener datos:', error);
+      });
+  }, [])
+
   return (
     <section className="py-5">
-      <div className="container px-5 mb-5">
-        <div className="text-center mb-5">
-          <h1 className="display-5 fw-bolder mb-0">
-            <span className="text-gradient d-inline">Projects</span>
-          </h1>
-        </div>
-        <div className="row gx-5 justify-content-center">
-          <div className="col-lg-11 col-xl-9 col-xxl-8">
-            {/* Project Card 1*/}
-            <div className="card overflow-hidden shadow rounded-4 border-0 mb-5">
+    <div className="container px-5 mb-5">
+      <div className="text-center mb-5">
+        <h1 className="display-5 fw-bolder mb-0">
+          <span className="text-gradient d-inline">Projects</span>
+        </h1>
+      </div>
+      <div className="row gx-5 justify-content-center">
+        <div className="col-lg-11 col-xl-9 col-xxl-8">
+          {proyectos.map((proyecto, index) => (
+            <div className="card overflow-hidden shadow rounded-4 border-0 mb-5" key={index}>
               <div className="card-body p-0">
                 <div className="d-flex align-items-center">
                   <div className="p-5">
-                    <h2 className="fw-bolder">Project Name 1</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Eius at enim eum illum aperiam placeat esse? Mollitia
-                      omnis minima saepe recusandae libero, iste ad
-                      asperiores! Explicabo commodi quo itaque! Ipsam!
-                    </p>
+                    <h2 className="fw-bolder">{proyecto.titulo}</h2>
+                    <p>{proyecto.descripcion}</p>
+                    <p>Fecha: {proyecto.fecha}</p>
+                    <p>Enlace: <a href={proyecto.url} target="_blank" rel="noopener noreferrer">{proyecto.url}</a></p>
                   </div>
                   <img
                     className="img-fluid"
-                    src="https://dummyimage.com/300x400/343a40/6c757d"
+                    src={proyecto.imagenes[0]} 
                     alt="..."
                   />
                 </div>
               </div>
             </div>
-            {/* Project Card 2*/}
-            <div className="card overflow-hidden shadow rounded-4 border-0">
-              <div className="card-body p-0">
-                <div className="d-flex align-items-center">
-                  <div className="p-5">
-                    <h2 className="fw-bolder">Project Name 2</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Eius at enim eum illum aperiam placeat esse? Mollitia
-                      omnis minima saepe recusandae libero, iste ad
-                      asperiores! Explicabo commodi quo itaque! Ipsam!
-                    </p>
-                  </div>
-                  <img
-                    className="img-fluid"
-                    src="https://dummyimage.com/300x400/343a40/6c757d"
-                    alt="..."
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-      {/* Call to action section*/}
-      <section className="py-5 bg-gradient-primary-to-secondary text-white">
-        <div className="container px-5 my-5">
-          <div className="text-center">
-            <h2 className="display-4 fw-bolder mb-4">
-              Let's build something together
-            </h2>
-            <Link
-              className="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder"
-              to="/contacto"
-            >
-              Contact me
-            </Link>
-          </div>
+    </div>
+    <section className="py-5 bg-gradient-primary-to-secondary text-white">
+      <div className="container px-5 my-5">
+        <div className="text-center">
+          <h2 className="display-4 fw-bolder mb-4">
+            Let's build something together
+          </h2>
+          <Link
+            className="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder"
+            to="/contacto"
+          >
+            Contact me
+          </Link>
         </div>
-      </section>
+      </div>
     </section>
+  </section>
   );
 };
 
