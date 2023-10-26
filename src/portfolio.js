48ff06a228c2d1c0eadd0809535ff6a264e14c41
47297659 <47297659@A-PHZ2-CIDI-012>
2023-10-26 10:35:00 -0300

    Avances

--- src/portfolio.js
+++ src/portfolio.js
@@ -341,7 +341,7 @@ const podcastSection = {
 };
 
 const contactInfo = {
-  title: emoji("Contact Me ☎️"),
+  title: emoji("Contacto ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   number: "+92-0000000000",
