bd0e0462b25b882672ba31ebcdd55a903e217052
47297659 <47297659@A-PHZ2-CIDI-012>
2023-10-26 09:02:20 -0300

    Base template

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import PageProyectos from "./pages/PageProyectos";
-import Experiencia from "./pages/Experiencia";
-import Resumen from "./pages/Resumen";
-import Contacto from "./pages/Contacto";
-import Favoritos from "./pages/Favoritos";
-import DetalleProyecto from "./components/DetalleProyectos";
+import { BrowserRouter } from "react-router-dom";
+import "./App.scss";
+import Main from "./containers/Main";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout><Home /></Layout>} />
-          <Route path="/proyectos" element={<Layout><PageProyectos /></Layout>} />
-          <Route path="/experiencia" element={<Layout><Experiencia /></Layout>} />
-          <Route path="/resumen" element={<Layout><Resumen /></Layout>} />
-          <Route path="/contacto" element={<Layout><Contacto /></Layout>} />
-          <Route path="/favoritos" element={<Layout><Favoritos /></Layout>} />
-          <Route path="/proyectos/:id" element={<Layout><DetalleProyecto/></Layout>} />
-
-        </Routes>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
